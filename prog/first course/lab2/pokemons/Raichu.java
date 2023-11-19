f0ec7889807ac711d4b2e1062e8b8ade80f57381
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-19 16:16:08 +0300

    Create Raichu.java

---
+++ prog/first course/lab2/pokemons/Raichu.java
@@ -0,0 +1,22 @@
+package pokemons;
+import ru.ifmo.se.pokemon.*;
+import moves.Thunderbolt;
+import moves.Wild_Charge;
+import moves.Nuzzle;
+import moves.Swagger;
+public class Raichu extends Pokemon {
+    public Raichu(String name, int lvl) {
+        super(name, lvl);
+        super.setType(Type.ELECTRIC);
+        super.setStats(60, 90, 55, 90, 80, 110);
+        //pokemon_moves
+        Wild_Charge wild_charge = new Wild_Charge(90, 100);
+        Thunderbolt thunderbolt = new Thunderbolt(90, 100);
+        Nuzzle nuzzle = new Nuzzle(20, 100);
+        Swagger swagger = new Swagger(20, 100);
+        super.setMove(wild_charge, thunderbolt, nuzzle, swagger);
+
+
+    }
+
+}
