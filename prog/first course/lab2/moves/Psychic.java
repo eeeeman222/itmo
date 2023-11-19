deced95db15e1a2aa9b8b0d55728954dcaba9f8c
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-19 16:03:40 +0300

    Create Psychic.java

---
+++ prog/first course/lab2/moves/Psychic.java
@@ -0,0 +1,18 @@
+package moves;
+
+import ru.ifmo.se.pokemon.*;
+
+public class Psychic extends SpecialMove {
+    public Psychic(double power, double acc) {
+        super(Type.PSYCHIC, power, acc);
+    }
+    @Override
+    protected void applyOppEffects(Pokemon opp) {
+        Effect ShadowBall_E = new Effect().chance(0.1).turns(1).stat(Stat.DEFENSE, -1);
+        opp.addEffect(ShadowBall_E);
+    }
+    @Override
+    protected String describe() {
+        return "атакует с помощью Psychic!";
+    }
+}
