7f47e94b89134e4c0b35471bf551cac483f10207
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-19 16:05:50 +0300

    Create Thunderbolt.java

---
+++ prog/first course/lab2/moves/Thunderbolt.java
@@ -0,0 +1,18 @@
+package moves;
+import ru.ifmo.se.pokemon.*;
+
+public class Thunderbolt extends SpecialMove {
+    public Thunderbolt(double power, double acc){
+        super(Type.ELECTRIC, power, acc);
+    }
+    @Override
+    protected void applyOppEffects(Pokemon opp) {
+        if(Math.random() < 0.1){
+            Effect.paralyze(opp);
+        }
+    }
+    @Override
+    protected java.lang.String describe() {
+        return "неожиданно атакует используя Thunderbolt!";
+    }
+}
