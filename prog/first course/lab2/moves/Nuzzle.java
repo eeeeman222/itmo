065ecdc31d3c611b41363482a0a56d68f95a42a3
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 15:04:24 +0300

    Update Nuzzle.java

--- prog/first course/lab2/moves/Nuzzle.java
+++ prog/first course/lab2/moves/Nuzzle.java
@@ -1,7 +1,7 @@
 package moves;
 import ru.ifmo.se.pokemon.*;
 public class Nuzzle extends PhysicalMove{
-    public Nuzzle(int power, int accuracy) {
+    public nuzzle(int power, int accuracy) {
         super(Type.ELECTRIC, power, accuracy);
     }
     protected void applyOppEffects(Pokemon opp) {
