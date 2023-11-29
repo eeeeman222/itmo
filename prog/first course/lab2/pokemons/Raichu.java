0036e661d0e582b47571d2ead0e826730b56b136
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 14:59:32 +0300

    Update Raichu.java

--- prog/first course/lab2/pokemons/Raichu.java
+++ prog/first course/lab2/pokemons/Raichu.java
@@ -10,11 +10,11 @@ public class Raichu extends Pokemon {
         super.setType(Type.ELECTRIC);
         super.setStats(60, 90, 55, 90, 80, 110);
         //pokemon_moves
-        Wild_Charge wild_charge = new Wild_Charge(90, 100);
+        Wild_Charge wildcharge = new WildCharge(90, 100);
         Thunderbolt thunderbolt = new Thunderbolt(90, 100);
         Nuzzle nuzzle = new Nuzzle(20, 100);
         Swagger swagger = new Swagger(20, 100);
-        super.setMove(wild_charge, thunderbolt, nuzzle, swagger);
+        super.setMove(wildcharge, thunderbolt, nuzzle, swagger);
 
 
     }
