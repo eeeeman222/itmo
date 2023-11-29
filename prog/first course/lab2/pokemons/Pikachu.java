4641a0d8f310eaf2b726ee4a19c85febaff421a7
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 14:59:10 +0300

    Update Pikachu.java

--- prog/first course/lab2/pokemons/Pikachu.java
+++ prog/first course/lab2/pokemons/Pikachu.java
@@ -12,7 +12,7 @@ public class Pikachu extends Pokemon {
 
         //pokemon_moves
 
-        Wild_Charge wild_charge = new Wild_Charge(90, 100);
+        WildCharge wildcharge = new WildCharge(90, 100);
         Thunderbolt thunderbolt = new Thunderbolt(90, 100);
         Nuzzle nuzzle = new Nuzzle(20, 100);
         super.setMove(wild_charge, thunderbolt, nuzzle);
