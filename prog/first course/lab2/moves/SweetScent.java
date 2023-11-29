2d28e17c18e0d0196351e5aadaa65d0bd361f065
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 14:57:25 +0300

    Update and rename Sweet_Scent.java to SweetScent.java

--- prog/first course/lab2/moves/SweetScent.java
+++ prog/first course/lab2/moves/SweetScent.java
@@ -1,13 +1,13 @@
 package moves;
 import ru.ifmo.se.pokemon.*;
-public class Sweet_Scent extends StatusMove {
-    public Sweet_Scent(double power, double acc) {
+public class SweetScent extends StatusMove {
+    public SweetScent(double power, double acc) {
         super(Type.NORMAL, power, acc);
     }
     @Override
     protected void applyOppEffects(Pokemon opp) {
-        Effect Sweet_Scent_E = new Effect().chance(1).turns(1).stat(Stat.EVASION, -1);
-        opp.addEffect(Sweet_Scent_E);
+        Effect SweetScentE = new Effect().chance(1).turns(1).stat(Stat.EVASION, -1);
+        opp.addEffect(Sweet_ScentE);
     }
     @Override
     protected String describe() {
