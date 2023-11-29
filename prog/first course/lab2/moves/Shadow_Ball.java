5abd8d429020145aab56c2a85274120bf157dc3e
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 14:56:19 +0300

    Update Shadow_Ball.java

--- prog/first course/lab2/moves/Shadow_Ball.java
+++ prog/first course/lab2/moves/Shadow_Ball.java
@@ -10,8 +10,8 @@ public class ShadowBall extends SpecialMove{
     }
     @Override
     protected void applyOppEffects(Pokemon opp) {
-        Effect ShadowBall_E = new Effect().chance(0.2).turns(1).stat(Stat.DEFENSE, -1);
-        opp.addEffect(ShadowBall_E);
+        Effect ShadowBallE = new Effect().chance(0.2).turns(1).stat(Stat.DEFENSE, -1);
+        opp.addEffect(ShadowBallE);
     }
 
 
