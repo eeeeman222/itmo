661509059baf66b9b70b63329a3e538ec070ab9c
eeeeman222 <145559780+eeeeman222@users.noreply.github.com>
2023-11-29 14:55:31 +0300

    Update Double_Team.java

--- prog/first course/lab2/moves/Double_Team.java
+++ prog/first course/lab2/moves/Double_Team.java
@@ -3,13 +3,13 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class Double_team extends StatusMove {
-    public Double_team(double power, double acc) {
+    public DoubleTeam(double power, double acc) {
         super(Type.NORMAL, power, acc);
     }
     @Override
     protected void applySelfEffects(Pokemon me) {
-        Effect Double_team_E = new Effect().chance(1).turns(1).stat(Stat.EVASION, +1);
-        me.addEffect(Double_team_E);
+        Effect DoubleTeamE = new Effect().chance(1).turns(1).stat(Stat.EVASION, +1);
+        me.addEffect(DoubleTeamE);
     }
     @Override
     protected String describe() {
